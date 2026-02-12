fd3b0967d32103ad4f8d1ad91e9c56e7edaa4cb0
Pooja Kumari <basupooja306@gmail.com>
2026-02-12 13:04:37 +0530

    Login page added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { useRouter } from "next/navigation";
-<<<<<<< HEAD
 export default function LoginPage() {
 
   const loginUser = async () => {
@@ -28,42 +27,7 @@ export default function LoginPage() {
         localStorage.setItem("token", data.token);
         router.push("/products");
 
-=======
-
-export default function LoginPage() {
-
-    const router = useRouter();
-
-    const loginUser = async () => {
-
-    try {
-        await fetch("http://127.0.0.1:8000/sanctum/csrf-cookie", {
-            method: "GET",
-            credentials: "include",
-        });
-
-  
-        const response = await fetch("http://127.0.0.1:8000/login", {
-        method: "POST",
-        credentials: "include",
-        headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-        },
-        body: JSON.stringify({
-            email: "Pooja@gmail.com",
-            password: "12345678",
-        }),
-        });
-        if (!response.ok) {
-            alert("Login Failed ❌");
-            return;
-        }
-
-        alert("Login Successful ✅");
-        router.push("/products");
-
->>>>>>> 8a04ed8 (Login page added)
+        
     } catch (error) {
     console.error("Login error:", error);
     alert("Something went wrong ❌");
