4503dcf6911670543a3f7d5e39083aee96603b06
Pooja Kumari <basupooja306@gmail.com>
2026-02-12 21:14:21 +0530

    User login  Sucessfully

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,27 +1,36 @@
 "use client";
-
+import { useRouter } from "next/navigation";
 export default function LoginPage() {
 
   const loginUser = async () => {
+    const router = useRouter();
+    try{
+        const response = await fetch("http://127.0.0.1:8000/api/login", {
+        method: "POST",
+        credentials: "include",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json",
+        },
+        body: JSON.stringify({
+            email: "Pooja@gmail.com",
+            password: "12345678",
+        }),
+        });
+        if (!response.ok) {
+            alert("Login Failed ❌");
+            return;
+        }
 
+        alert("Login Successful ✅");
+        const data = await response.json();
+        localStorage.setItem("token", data.token);
+        router.push("/products");
 
-    await fetch("http://127.0.0.1:8000/sanctum/csrf-cookie", {
-      credentials: "include",
-    });
-
-  
-    await fetch("http://127.0.0.1:8000/login", {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-      body: JSON.stringify({
-        email: "Pooja@gmail.com",
-        password: "12345678",
-      }),
-    });
+    } catch (error) {
+    console.error("Login error:", error);
+    alert("Something went wrong ❌");
+    }
   };
 
   return (
